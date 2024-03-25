6d2fb5ee57235f7023f177a4baaf207ea30af4e9
Pedro Barbosa <159566197+PedroBarbosaIF@users.noreply.github.com>
2024-03-24 23:35:22 -0300

    Add files via upload

---
+++ CAP-03/Exercicios_Propostos/QP20/src/br/edu/principal/Principal.java
@@ -0,0 +1,24 @@
+package br.edu.principal;
+
+import java.util.Scanner;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Qual é o valor do ângulo formado entre a escada e a parede? ");
+		int angulo = sc.nextInt();
+		
+		System.out.print("Qual é o valor da distância até a parede? ");
+		float distancia = sc.nextInt();
+		
+		double graus_radianos = angulo * Math.PI / 180;
+		double medida_escada = distancia/ Math.cos(graus_radianos);
+		float medida_escada2 = (float)medida_escada;
+		
+		System.out.print("Esta é a medida da escada: "+ medida_escada2);
+		
+	}
+
+}
