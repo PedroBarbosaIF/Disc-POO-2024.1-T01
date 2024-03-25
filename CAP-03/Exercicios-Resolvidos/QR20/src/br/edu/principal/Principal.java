796c49a1e0d251f86aaa3b43e0aec228052bd45e
Pedro Barbosa <159566197+PedroBarbosaIF@users.noreply.github.com>
2024-03-24 23:35:01 -0300

    Add files via upload

---
+++ CAP-03/Exercicios-Resolvidos/QR20/src/br/edu/principal/Principal.java
@@ -0,0 +1,24 @@
+package br.edu.principal;
+
+import java.util.Scanner;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Qual a altura da parede? ");
+		float altura = sc.nextFloat();
+		
+		System.out.print("Qual o valor do ângulo formado? ");
+		int angulo = sc.nextInt();
+		
+		double radiano = angulo * Math.PI / 180;
+		double escada = altura / Math.sin(radiano);
+		
+		float escada2 = (float)escada;
+		
+		System.out.print("Esta é a altura da escada: "+ escada2);
+	}
+
+}
