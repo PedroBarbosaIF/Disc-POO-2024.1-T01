8700e5e63c82e5ad92ca3c305b59f3f43f2427b0
Pedro Barbosa <159566197+PedroBarbosaIF@users.noreply.github.com>
2024-03-29 12:15:56 -0300

    Update Principal.java

--- CAP-03/Exercicios-Resolvidos/QR16/src/br/edu/principal/Principal.java
+++ CAP-03/Exercicios-Resolvidos/QR16/src/br/edu/principal/Principal.java
@@ -5,6 +5,14 @@ import java.util.Scanner;
 public class Principal {
 
 	public static void main(String[] args) {
+		/* Questão QR16: Faça um programa que receba o número de horas trabalhadas e o valor do salário mínimo, calcule e
+		mostre o salário a receber, seguindo estas regras:
+		a) a hora trabalhada vale a metade do salário mínimo.
+		b) o salário bruto equivale ao número de horas trabalhadas multiplicado pelo valor da hora trabalhada.
+		c) o imposto equivale a 3% do salário bruto.
+		d) o salário a receber equivale ao salário bruto menos o imposto.
+		*/
+		
 		Scanner sc = new Scanner(System.in);
 		
 		float horas_trabalhadas, valor_salario_minimo,valor_horas_trabalhadas,
