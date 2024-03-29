447e883b9df9473ecfa2ed1d92f3b668f8539ab0
Pedro Barbosa <159566197+PedroBarbosaIF@users.noreply.github.com>
2024-03-29 13:04:56 -0300

    Update Principal.java

--- CAP-03/Exercicios-Propostos/QP20/src/br/edu/principal/Principal.java
+++ CAP-03/Exercicios-Propostos/QP20/src/br/edu/principal/Principal.java
@@ -5,6 +5,11 @@ import java.util.Scanner;
 public class Principal {
 
 	public static void main(String[] args) {
+		/* Questão proposta QP20: Faça um programa que receba a medida do ângulo formado por uma escada apoiada no chão e a dis-
+		tância em que a escada está da parede, calcule e mostre a medida da escada para que se possa alcançar
+		sua ponta.
+		*/
+		
 		Scanner sc = new Scanner(System.in);
 		
 		System.out.print("Qual é o valor do ângulo formado entre a escada e a parede? ");
