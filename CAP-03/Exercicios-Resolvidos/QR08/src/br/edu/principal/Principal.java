014596c1883ef7cffe07b849b8338d81ea11a7cd
Pedro Barbosa <159566197+PedroBarbosaIF@users.noreply.github.com>
2024-03-29 10:21:46 -0300

    Update Principal.java

--- CAP-03/Exercicios-Resolvidos/QR08/src/br/edu/principal/Principal.java
+++ CAP-03/Exercicios-Resolvidos/QR08/src/br/edu/principal/Principal.java
@@ -5,6 +5,9 @@ import java.util.Scanner;
 public class Principal {
 
 	public static void main(String[] args) {
+		/*Questão QR0: Faça um programa que receba o valor de um depósito e o valor da taxa de juros, calcule e mostre o
+		valor do rendimento e o valor total depois do rendimento.*/	
+		
 		Scanner sc = new Scanner(System.in);
 		
 		System.out.print("Digite seu depósito: ");
