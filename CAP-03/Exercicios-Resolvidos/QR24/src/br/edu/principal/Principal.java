b849c0171def47edf2cc1392bff74f0eb1c447dd
Pedro Barbosa <159566197+PedroBarbosaIF@users.noreply.github.com>
2024-03-29 12:27:19 -0300

    Update Principal.java

--- CAP-03/Exercicios-Resolvidos/QR24/src/br/edu/principal/Principal.java
+++ CAP-03/Exercicios-Resolvidos/QR24/src/br/edu/principal/Principal.java
@@ -5,6 +5,11 @@ import java.util.Scanner;
 public class Principal {
 
 	public static void main(String[] args) {
+		/* Questão QR24: Faça um programa que receba uma hora formada por hora e minutos (um número real), calcule e
+		mostre a hora digitada apenas em minutos. Lembre-se de que:
+		■■ para quatro e meia, deve-se digitar 4.30;
+		■■ os minutos vão de 0 a 59.*/
+		
 		Scanner sc = new Scanner(System.in);
 		
 		System.out.print("Digite uma hora(Exemplo: quatro e meia = 4,30 ): ");
