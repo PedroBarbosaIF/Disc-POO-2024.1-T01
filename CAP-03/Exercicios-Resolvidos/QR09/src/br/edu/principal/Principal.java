03ca0a76801f4d427b30ea580dc18d6eb5b2ac29
Pedro Barbosa <159566197+PedroBarbosaIF@users.noreply.github.com>
2024-03-29 10:26:21 -0300

    Update Principal.java

--- CAP-03/Exercicios-Resolvidos/QR09/src/br/edu/principal/Principal.java
+++ CAP-03/Exercicios-Resolvidos/QR09/src/br/edu/principal/Principal.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Principal {
 
 	public static void main(String[] args) {
-		/*Faça um programa que calcule e mostre a área de um triângulo. Sabe-se que: Área = (base * altura)/2.*/
+		/*Questão QR09: Faça um programa que calcule e mostre a área de um triângulo. Sabe-se que: Área = (base * altura)/2.*/
 		
 		float base, altura, area;
 		
