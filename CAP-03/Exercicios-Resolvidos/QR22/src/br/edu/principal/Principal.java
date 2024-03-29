0bea1c80cf9e92016b234b8662447b6defa71ade
Pedro Barbosa <159566197+PedroBarbosaIF@users.noreply.github.com>
2024-03-29 12:25:39 -0300

    Update Principal.java

--- CAP-03/Exercicios-Resolvidos/QR22/src/br/edu/principal/Principal.java
+++ CAP-03/Exercicios-Resolvidos/QR22/src/br/edu/principal/Principal.java
@@ -5,6 +5,12 @@ import java.util.Scanner;
 public class Principal {
 
 	public static void main(String[] args) {
+		/* Questão QR22: Sabe-se que o quilowatt de energia custa um quinto do salário mínimo. Faça um programa que rece-
+		ba o valor do salário mínimo e a quantidade de quilowatts consumida por uma residência. Calcule e mostre:
+		a) o valor de cada quilowatt;
+		b) o valor a ser pago por essa residência;
+		c) o valor a ser pago com desconto de 15%.*/
+		
 		Scanner sc = new Scanner(System.in);
 		
 		System.out.print("Quual o valor do salário mínimo? ");
