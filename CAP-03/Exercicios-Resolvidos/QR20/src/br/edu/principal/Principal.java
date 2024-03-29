cce3b5eb95b97c52d274d03af411cc2d12d31cee
Pedro Barbosa <159566197+PedroBarbosaIF@users.noreply.github.com>
2024-03-29 12:21:35 -0300

    Update Principal.java

--- CAP-03/Exercicios-Resolvidos/QR20/src/br/edu/principal/Principal.java
+++ CAP-03/Exercicios-Resolvidos/QR20/src/br/edu/principal/Principal.java
@@ -5,6 +5,10 @@ import java.util.Scanner;
 public class Principal {
 
 	public static void main(String[] args) {
+		/* Questão QR20: Faça um programa que receba a medida do ângulo (em graus) formado por uma escada apoiada no
+		chão e encostada na parede e a altura da parede onde está a ponta da escada. Calcule e mostre a me-
+		dida dessa escada.
+		*/
 		Scanner sc = new Scanner(System.in);
 		
 		System.out.print("Qual a altura da parede? ");
