841f63bacd77afd786982c41e302fed349e5ef58
Pedro Barbosa <159566197+PedroBarbosaIF@users.noreply.github.com>
2024-03-29 10:18:58 -0300

    Update Principal.java

--- CAP-03/Exercicios-Resolvidos/QR07/src/br/edu/principal/Principal.java
+++ CAP-03/Exercicios-Resolvidos/QR07/src/br/edu/principal/Principal.java
@@ -5,6 +5,9 @@ import java.util.Scanner;
 public class Principal {
 
 	public static void main(String[] args) {
+		/*Questão QR07: Faça um programa que receba o salário base de um funcionário, calcule e mostre seu salário a receber, 
+  		sabendo-se que o funcionário tem gratificação de R$ 50 e paga imposto de 10% sobre o salário base.*/	
+		
 		Scanner sc = new Scanner(System.in);
 		
 		System.out.print("Dgite seu salário base: ");
