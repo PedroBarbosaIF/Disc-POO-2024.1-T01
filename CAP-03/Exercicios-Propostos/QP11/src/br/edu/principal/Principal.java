888c3cac2e567a21eee0c2ed1554b5bfe0cb56cf
Luiza Monteiro <159566275+luizamtro@users.noreply.github.com>
2024-03-30 08:23:51 -0300

    Add files via upload

---
+++ CAP-03/Exercicios-Propostos/QP11/src/br/edu/principal/Principal.java
@@ -0,0 +1,29 @@
+/* 11. Faça um programa que calcule e mostre a área de um losango. Sabe-se que: A = (diagonal maior * dia-
+		gonal menor)/2.8*/
+
+		package br.edu.principal;
+
+		import java.util.Scanner;
+
+		public class Principal {
+
+		  public static void main(String[] args) {
+		    Scanner sc = new Scanner (System.in);
+
+
+		       Double área_losang, diag_maior, diag_menor;
+
+
+		       System.out.println("Digite a diagonal maior do losango: ");
+		       diag_maior = sc.nextDouble();
+
+		       System.out.println("Digite a diagonal menor do losango: ");
+		       diag_menor = sc.nextDouble();
+
+		       área_losang = (diag_maior * diag_menor)/2;
+		         System.out.println("A área do losango é: " + área_losang + "cm²");
+
+		         sc.close();
+		  }
+
+		}
